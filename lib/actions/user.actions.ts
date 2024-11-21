4eb049e7f5e72eff33e86d91a3c0482738f7d5dd
MajorVick <126904019+MajorVick@users.noreply.github.com>
2024-11-21 05:28:53 +0000

    added user authentication

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,27 +1,67 @@
-'use server'
+"use server";
 
-import { Query } from "appwrite";
+import { Query, ID } from "appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
 
 const getUserByEmail = async (email: string) => {
-    const { databases } = await createAdminClient();
+  const { databases } = await createAdminClient();
 
-    const result = await databases.listDocuments(
-        appwriteConfig.databaseId, 
-        appwriteConfig.usersCollectionId,
-        queries: [Query.equal("email", [email])],
-    );
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
+  );
 
-    return result.total > 0 ? result.documents[0] : null;
-}
+  return result.total > 0 ? result.documents[0] : null;
+};
 
-const createAccount = async ({
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+export const createAccount = async ({
   fullName,
   email,
 }: {
   fullName: string;
   email: string;
 }) => {
-    const existingUser = await getUserByEmail(email);
+  const existingUser = await getUserByEmail(email);
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("Failed to send an OTP");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://png.pngtree.com/png-clipart/20220121/ourmid/pngtree-cartoon-hand-drawn-default-avatar-cat-png-image_4278361.png",
+        accountId,
+      }
+    );
+  }
+
+  return parseStringify({ accountId });
 };
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // enable server actions
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
+  // disable strict mode for now
+  reactStrictMode: false,
+
+  // configure header for cors
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ];
+  },
   /* config options here */
   webpack: (config, { dev }) => {
     if (dev) {
-      config.cache= false;
+      config.cache = false;
     }
     return config;
+  },
+
+  // configure domain for github codespaces
+  images: {
+    domains: ["*.app.github.dev"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.app.github.dev",
+      },
+    ],
   },
 };
 
